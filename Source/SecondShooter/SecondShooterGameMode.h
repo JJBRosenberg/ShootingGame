d7001582445db896c60782f4e9ebddde7815f1ad
jj <jjrosenberg122@gmail.com>
2022-10-10 16:01:12 +0200

    Big asset push
    Pushing all the project so far

---
+++ Source/SecondShooter/SecondShooterGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SecondShooterGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASecondShooterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASecondShooterGameMode();
+};
+
+
+
